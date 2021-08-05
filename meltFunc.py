e584d021e129a054977d268f5dcd8d14c6acd21f
Danny5109 <dannythomson510@gmail.com>
2021-08-05 12:37:09 +0530

    tried pivot func

--- meltFunc.py
+++ meltFunc.py
@@ -1,6 +1,24 @@
+from numpy import pi
 import pandas as pd
 
 df = pd.read_csv("address.csv")
-df1= df.melt(id_vars=['Name'], var_name='Item', value_name='Value')
-df1.to_csv("address.csv", index = False)
-print(df1)
+
+col_len = len(df.columns)
+
+def meltFunc():
+    df1= df.melt(id_vars=['Name'], var_name='Item', value_name='Value')
+    df1.to_csv("address.csv", index = False)
+    print(df1)
+
+#unmelting
+def pivotFunc():
+    df2 = df.pivot(index = 'Name', columns = 'Item')
+    df2 = df2['Value'].reset_index()
+    df2.columns.name = None
+    df2.to_csv("address.csv", index = False)
+    print(df2)
+
+if col_len == 3:
+    pivotFunc()
+else:
+    meltFunc()
